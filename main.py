e048bffacf0200abd984e2df3034cc9e34d178aa
Sushil <sushilrawat1720@gmail.com>
2023-04-27 23:29:22 +0530

    added model and ui

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as  st
+import pickle
+import requests
+
+st.title("Movie Recommendation System")
+movies=pickle.load(open('movies.pkl','rb'))
+movie_list_title=movies['title'].values
+similariy=pickle.load(open('similarity.pkl','rb'))
+
+def fetch_poster(movie_id):
+    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=62d06fc92abf22d0a252da28f2a7f61a&language=en-US'.format(movie_id))
+    data=response.json()
+    print(data)
+    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similariy[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_poster=[ ]
+
+    for i in movies_list:
+        print(i[0])
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_poster.append(fetch_poster(movies.iloc[i[0]].movie_id))
+    return recommended_movies,recommended_movies_poster
+
+selected_movie_name = st.selectbox(
+    'How would you like to be contacted?',
+   movie_list_title)
+
+if st.button('Recommend '):
+    names,poster=recommend(selected_movie_name)
+    col1, col2, col3,col4,col5  = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(poster[0])
+    with col2:
+        st.text(names[1])
+        st.image(poster[1])
+    with col3:
+        st.text(names[2])
+        st.image(poster[2])
+    with col4:
+        st.text(names[3])
+        st.image(poster[3])
+    with col5:
+        st.text(names[4])
+        st.image(poster[4])
+
+    # for i in recommendation:
+    #     st.write(i)
+    
